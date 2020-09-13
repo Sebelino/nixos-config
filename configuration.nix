d5b0dfc60b9ec3cc8ef0f590bade1a39242a4273
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-13 21:55:44 +0000

    Configure system to use KDE desktop for now

--- configuration.nix
+++ configuration.nix
@@ -91,16 +91,16 @@
   # hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   users.mutableUsers = false;
 
