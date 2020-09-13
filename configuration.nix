53c4c6c0b11abbfb6177927ecdc9f1a12483fe9d
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-13 20:29:57 +0000

    Enable gnupg agent to be able to use git-crypt

--- configuration.nix
+++ configuration.nix
@@ -67,11 +67,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  #   pinentryFlavor = "gnome3";
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "curses";
+  };
 
   # List services that you want to enable:
 
