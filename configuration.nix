e8f78796080e0f5898a772c2d53d4d24d4ea6d44
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-13 20:33:14 +0000

    Allow connecting to this host over sftp

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.allowSFTP = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
