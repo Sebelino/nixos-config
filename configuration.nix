3f010c9631138025eb7144cd5d669aea896d2faf
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-14 05:49:50 +0000

    Setting layout like this doesn't seem to work but it is worth a try

--- configuration.nix
+++ configuration.nix
@@ -92,7 +92,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.layout = "us";
+  services.xserver.layout = "se";
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable touchpad support.
