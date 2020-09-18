f694354ac4ed10809374f63722042c8bce118167
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-18 16:50:46 +0200

    refactor: Remove commented out proxy section

--- configuration.nix
+++ configuration.nix
@@ -38,10 +38,6 @@
   networking.interfaces.enp0s31f6.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
