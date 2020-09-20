55ec087f043a03b7e741ac7adeff7d93b887a537
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-20 16:07:01 +0200

    feat(gpu): Enable use of Nvidia PRIME Optimus 3D controller

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,14 @@ let
     url = "https://github.com/rycee/home-manager.git";
     rev = "9b1b55ba0264a55add4b7b4e022bdc2832b531f6";
   };
+
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in
 {
   imports =
@@ -91,6 +99,7 @@ in
     acpi
     slack
     wget
+    nvidia-offload
   ];
 
   fonts.fonts = with pkgs; [
@@ -173,6 +182,8 @@ in
 
       windowManager.xmonad.enable = true;
       windowManager.xmonad.enableContribAndExtras = true;
+
+      videoDrivers = [ "nvidia" ];
     };
   };
 
@@ -181,6 +192,17 @@ in
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    # See https://nixos.wiki/wiki/Nvidia for how to set these
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:60:0:0";
+  };
 
   # Tell certain packages to be built with pulseaudio support if available
   nixpkgs.config.pulseaudio = true;
