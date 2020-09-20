8200b16b7d308b989b4b296fc262dd835d7e29af
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-20 11:36:31 +0200

    refactor(home): Moved prezto shell aliases to separate nix file

--- home.nix
+++ home.nix
@@ -27,30 +27,7 @@
     zsh = {
       enable = true;
       autocd = true;
-      shellAliases = {
-        # Commit (c)
-        gc = "git commit --verbose";
-        gca = "git commit --verbose --all";
-        gcm = "git commit --message";
-        gcS = "git commit -S --verbose";
-        gcSa = "git commit -S --verbose --all";
-        gcSm = "git commit -S --message";
-        gcam = "git commit --all --message";
-        gco = "git checkout";
-        gcO = "git checkout --patch";
-        gcf = "git commit --amend --reuse-message HEAD";
-        gcSf = "git commit -S --amend --reuse-message HEAD";
-        gcF = "git commit --verbose --amend";
-        gcSF = "git commit -S --verbose --amend";
-        gcp = "git cherry-pick --ff";
-        gcP = "git cherry-pick --no-commit";
-        gcr = "git revert";
-        gcR = "git reset 'HEAD^'";
-        gcs = "git show";
-        gcl = "git-commit-lost";
-        gcy = "git cherry -v --abbrev";
-        gcY = "git cherry -v";
-      };
+      shellAliases = import ./shell-aliases.nix;
     };
     urxvt = {
       enable = true;
---
+++ shell-aliases.nix
@@ -0,0 +1,24 @@
+{
+  # Commit (c)
+  gc = "git commit --verbose";
+  gca = "git commit --verbose --all";
+  gcm = "git commit --message";
+  gcS = "git commit -S --verbose";
+  gcSa = "git commit -S --verbose --all";
+  gcSm = "git commit -S --message";
+  gcam = "git commit --all --message";
+  gco = "git checkout";
+  gcO = "git checkout --patch";
+  gcf = "git commit --amend --reuse-message HEAD";
+  gcSf = "git commit -S --amend --reuse-message HEAD";
+  gcF = "git commit --verbose --amend";
+  gcSF = "git commit -S --verbose --amend";
+  gcp = "git cherry-pick --ff";
+  gcP = "git cherry-pick --no-commit";
+  gcr = "git revert";
+  gcR = "git reset 'HEAD^'";
+  gcs = "git show";
+  gcl = "git-commit-lost";
+  gcy = "git cherry -v --abbrev";
+  gcY = "git cherry -v";
+}
