23e181667b3d2ad7935ea9b8fcc52885adcd5de0
Sebastian Olsson <sebastian.olsson@tink.se>
2020-09-20 14:02:22 +0200

    feat(terminal): Decrease font size 14 -> 12

--- home.nix
+++ home.nix
@@ -30,7 +30,7 @@
     urxvt = {
       enable = true;
       fonts = [
-        "xft:Inconsolata for Powerline:size=14"
+        "xft:Inconsolata for Powerline:size=12"
       ];
       scroll.bar.enable = false;
       scroll.lines = 20000;
