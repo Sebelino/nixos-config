abc8d68238b6235b255e8103c785aa245ab9e3a0
Sebastian Olsson <sebastian.olsson@tink.se>
2020-11-10 16:43:49 +0100

    fix(fan): Update sensors after reboot

--- configuration.nix
+++ configuration.nix
@@ -155,12 +155,6 @@ in {
         hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon6/temp3_input
         hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon6/temp4_input
         hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon6/temp5_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp10_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp12_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp13_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp14_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp15_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp16_input
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp1_input
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp2_input
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp3_input
@@ -168,7 +162,6 @@ in {
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp5_input
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp6_input
         hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp7_input
-        hwmon /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp9_input
         hwmon /sys/devices/virtual/thermal/thermal_zone3/hwmon4/temp1_input
         hwmon /sys/devices/virtual/thermal/thermal_zone4/hwmon5/temp1_input
         hwmon /sys/devices/virtual/thermal/thermal_zone7/hwmon7/temp1_input
