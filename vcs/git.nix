d76107b6231df70a3b05ed5f47dfd3aceef9266f
Sebastian Olsson <sebastian.olsson@tink.se>
2020-12-25 11:08:55 +0100

    refactor(vcs): Manage git aliases with home-manager

--- vcs/git.nix
+++ vcs/git.nix
@@ -1,5 +1,12 @@
 {
   enable = true;
   userName = "Sebastian Olsson";
+  aliases = {
+    pr-checkout = ''
+      !f() { git fetch upstream pull/"$1"/head:PR-"$1"; git checkout PR-"$1"; }; f'';
+    rewritebranch = "!f() { git rebase -i $(git merge-base master HEAD); }; f";
+    diffcommit = "!f(){ git diff HEAD~$(($1 + 1)) HEAD~$1; }; f";
+    diffcommitn = "!f(){ git diff HEAD~$(($1 + 1)) HEAD~$1 --name-only; }; f";
+  };
   ignores = [ "gitignore/" ".ijwb" ];
 }
