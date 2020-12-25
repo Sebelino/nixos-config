d5c91e4977886b422e4460b0777b0f68c504c5dd
Sebastian Olsson <sebastian.olsson@tink.se>
2020-12-25 12:04:48 +0100

    refactor(terminal): Move xresources to subdirectory

--- home.nix
+++ home.nix
@@ -33,20 +33,7 @@
     '';
   };
 
-  xresources.properties = {
-    "URxvt.background" = "rgba:0000/0000/0200/c800";
-    "URxvt.foreground" = "white";
-    "URxvt.keysym.Shift-Up" = "command:\\033]720;1\\007";
-    "URxvt.keysym.Shift-Down" = "command:\\033]721;1\\007";
-    "URxvt.borderless" = true;
-    "URxvt.highlightColor" = "#d01018";
-    "URxvt.depth" = 32; # Enables transparency together with xcompmgr -c
-    "URxvt.keysym.M-0xe5" = "perl:keyboard-select:search";
-    "URxvt.perl-ext-common" =
-      "default,clipboard,keyboard-select,selection-to-clipboard";
-    "URxvt.perl-lib" = "/home/sebelino/nixos-config/urxvt-perl";
-    "URxvt.fading" = "50";
-  };
+  xresources.properties = import ./terminal/xresources.nix;
 
   gtk = {
     enable = true;
