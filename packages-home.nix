d10a8e76f7aee2338f18755cfb959b7e77b3510d
Sebastian Olsson <sebastian.olsson@tink.se>
2020-12-25 11:43:42 +0100

    refactor(packages-home): Extract packgaes to separate file

---
+++ packages-home.nix
@@ -0,0 +1,91 @@
+{ pkgs }: with pkgs; [
+  ack
+  acpilight
+  arandr
+  awscli2
+  bazel-watcher
+  bind # Provides dig
+  buildifier
+  chromium
+  citrix_workspace
+  cmus
+  dconf
+  dive
+  dmenu
+  docker-compose
+  dos2unix
+  drive
+  fceux
+  feh
+  ffmpeg
+  file
+  fluidsynth
+  fzf
+  gcc
+  gephi
+  gettext # msgfmt
+  gimp
+  git
+  git-crypt
+  glxinfo
+  gnumake # Needed to build Bazel projects (python toolchain)
+  gnupg
+  go
+  google-cloud-sdk
+  gptfdisk
+  graphviz
+  gzdoom
+  imagemagick
+  inkscape
+  jdk11
+  jq
+  k4dirstat
+  keybase
+  keybase-gui
+  libxml2 # Needed for infra's scripts
+  llvmPackages.bintools
+  lm_sensors
+  mariadb-client
+  maven
+  mednafen
+  mednaffe
+  minikube
+  mkpasswd
+  mplayer
+  mpv
+  ncat # Provides nmap
+  nix-index # Example usage: nix-locate zlib.h
+  nix-prefetch-git
+  nixfmt
+  openssl
+  p7zip
+  parted
+  pavucontrol
+  pciutils
+  pcsx2
+  pinentry-curses
+  postman
+  ppsspp
+  python27
+  python39
+  ranger
+  scrot
+  shutter
+  ssm-session-manager-plugin
+  terraform_0_13
+  thunderbird
+  topydo
+  torbrowser
+  transmission-gtk
+  tree
+  unar # unrar replacement
+  unzip
+  xcompmgr
+  xmobar
+  xorg.transset
+  xorg.xdpyinfo
+  xorg.xev
+  xorg.xhost # Needed for infra's scripts
+  xorg.xkbcomp
+  zathura
+]
