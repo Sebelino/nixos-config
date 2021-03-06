cd94df8a97fa17228ab6b69bdd9c6b0a49b6843b
Sebastian Olsson <sebastian.olsson@tink.se>
2021-03-06 16:18:30 +0100

    fix(bin): Fix keyboard layout toggling script
    Darn, I just realized that all this work setting up this "sesebel"
    layout has been for pretty much nothing. I never had to create a
    slightly altered variant of the "se" layout after all. All that matters
    is that the "section" key is the same on both solemak and the
    alternative layout.
    
    But eh, let's keep it as it is. It might be good for reproducibility's
    sake to make a "pinned" copy anyway. Or maybe I would want to change the
    sesebel layout in the future, I dunno.

--- bin/toggle_solemak.sh
+++ bin/toggle_solemak.sh
@@ -5,7 +5,6 @@ out=`setxkbmap -query | grep solemak`
 if [ -z "$out" ]; then
     setxkbmap solemak
 else
-    # TODO Create this one. Same as SE except the button for toggling is the same as for Solemak.
-    setxkbmap se_sebelino
+    setxkbmap sesebel
 fi
 
