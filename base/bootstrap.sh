fa504985557470ab5f9808f6bec12f45c7a5c436
Sebastian Olsson <sebelino7@gmail.com>
2021-08-08 16:24:28 +0200

    feat(base): Download partitioning script from github

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -7,8 +7,6 @@ set -x
 wifi_passphrase="$1"
 luks_passphrase="$2"
 
-mv /mnt/bootstrap.sh /mnt/partitioning.sh /tmp/
-
 SSID="Olssons-5G"
 
 loadkeys /etc/kbd/keymaps/i386/qwerty/sv-latin1.map.gz
@@ -16,4 +14,7 @@ loadkeys /etc/kbd/keymaps/i386/qwerty/sv-latin1.map.gz
 echo "$wifi_passphrase" | wpa_passphrase "$SSID" | tee /etc/wpa_supplicant.conf
 systemctl restart wpa_supplicant.service
 
-bash /tmp/partitioning.sh "$luks_passphrase"
+nix-env --install git
+git clone https://github.com/Sebelino/nixos-config /tmp/nixos-config
+
+bash /tmp/nixos-config/base/partitioning.sh "$luks_passphrase"
