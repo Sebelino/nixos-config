0bb7d1ad99b7910ac0c9d857134452ffda935adc
Sebastian Olsson <sebelino7@gmail.com>
2021-08-08 16:13:08 +0200

    feat(base): Improve bootstrap script

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -2,15 +2,18 @@
 
 set -euo pipefail
 
-luks_passphrase="$1"
+set -x
+
+wifi_passphrase="$1"
+luks_passphrase="$2"
+
+mv /mnt/bootstrap.sh /mnt/partitioning.sh /tmp/
 
 SSID="Olssons-5G"
 
-sudo su
-
 loadkeys /etc/kbd/keymaps/i386/qwerty/sv-latin1.map.gz
 
-wpa_passphrase "$SSID" | tee /etc/wpa_supplicant.conf
+echo "$wifi_passphrase" | wpa_passphrase "$SSID" | tee /etc/wpa_supplicant.conf
 systemctl restart wpa_supplicant.service
 
-bash ./partitioning.sh "$luks_passphrase"
+bash /tmp/partitioning.sh "$luks_passphrase"
