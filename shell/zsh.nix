aa3c9e826bd49c6ed080af27fdf06a39aa5bd786
Sebastian Olsson <sebelino7@gmail.com>
2021-09-04 13:55:40 +0200

    feat(shell): Make nix-shell use bash -> zsh

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -51,6 +51,16 @@
         sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
       };
     }
+    {
+      name = "zsh-nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = pkgs.fetchFromGitHub {
+        owner = "chisui";
+        repo = "zsh-nix-shell";
+        rev = "v0.4.0";
+        sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+      };
+    }
   ];
   oh-my-zsh = {
     enable = true;
