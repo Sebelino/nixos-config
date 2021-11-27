8e5791b65498a277b164aceca16c8bab3286b0d4
Sebastian Olsson <sebelino7@gmail.com>
2021-11-27 18:48:10 +0100

    fix(base): Correct nixos-generate-config output dir

--- base/partitioning.sh
+++ base/partitioning.sh
@@ -84,7 +84,7 @@ git clone https://github.com/Sebelino/nixos-config
 install_dir="${BASE_DIR}/nixos-config/base"
 nixos_dir="${install_dir}/etc/nixos"
 
-nixos-generate-config --root "/mnt" --dir "/tmp/nixos-config/base/etc/nixos"
+nixos-generate-config --root "/mnt" --dir "${BASE_DIR#/mnt}/nixos-config/base/etc/nixos"
 echo "\"${lvmroot_uuid}\"" >> "${nixos_dir}/hardware-lvmroot-uuid.nix"
 
 mkdir -p /mnt/etc/nixos
