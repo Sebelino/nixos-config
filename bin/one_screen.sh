b0daa81b26e160603c86c3608ee47a45cf139a40
Sebastian Olsson <sebelino7@gmail.com>
2022-06-04 10:54:53 +0200

    fix(bin): Shut off more monitors

--- bin/one_screen.sh
+++ bin/one_screen.sh
@@ -6,10 +6,12 @@ set -x
 
 xrandr
 xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --primary \
+       --output HDMI-2 --off \
        --output HDMI-1-1 --off \
        --output HDMI-1-2 --off \
        --output DP-1-1 --off \
        --output DP-1-2 --off \
+       --output DP-2 --off \
        --verbose
 
 pkill xmobar ; xmobar &
