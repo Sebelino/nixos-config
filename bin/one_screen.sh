41f04e8cce9aa4dd19ff238cd1c1d6f5b426e3be
Sebastian Olsson <sebelino7@gmail.com>
2022-06-06 11:04:07 +0200

    fix(bin): Use laptop monitor correctly

--- bin/one_screen.sh
+++ bin/one_screen.sh
@@ -6,6 +6,7 @@ set -x
 
 xrandr
 xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --primary \
+       --output eDP-1 --mode 1920x1080 --pos 0x0 --primary \
        --output HDMI-2 --off \
        --output HDMI-1-1 --off \
        --output HDMI-1-2 --off \
