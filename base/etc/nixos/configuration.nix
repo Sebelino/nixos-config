043d1242b281f189987ca7ed3d7f658e3227c9ac
System administrator <sebelino7@gmail.com>
2022-07-13 18:48:42 +0000

    feat(base): Add basic necessities

--- base/etc/nixos/configuration.nix
+++ base/etc/nixos/configuration.nix
@@ -18,7 +18,52 @@
   networking.useDHCP = false;
   networking.interfaces.wlp2s0.useDHCP = true;
   networking.wireless.enable = true;
+  networking.wireless.networks = {
+   "Olssons-5G" = { psk = "***"; };
+   "Kirijo Group-5G" = { psk = "***"; };
+  };
   services.xserver.enable = true;
+
+  services.openssh.enable = true;
+  services.openssh.allowSFTP = true;
+  services.openssh.permitRootLogin = "yes";
+
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "curses";
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "sebelino" ];
+      commands = [{
+        command = "/run/current-system/sw/bin/nixos-rebuild switch";
+        options = [ "SETENV" "NOPASSWD" ];
+      }];
+    }
+  ];
+
+  users.users.sebelino = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  users.mutableUsers = true;
+
+  users.extraUsers.sebelino = {
+    shell = pkgs.zsh;
+    extraGroups = [ "docker" "video" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    git-crypt
+    gnupg
+    pinentry
+    pinentry-curses
+  ];
+
   system.stateVersion = "21.11";
 }
 
