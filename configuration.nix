3d21fc0913fadfca660e6588f798f39d49473c91
Sebastian Olsson <sebelino7@gmail.com>
2022-07-16 15:31:09 +0200

    refactor(system): Restructure config

--- configuration.nix
+++ configuration.nix
@@ -59,7 +59,9 @@ in {
 
   networking = {
     hostName = "zenia"; # Define your hostname.
-    wireless.enable = true; # Enables wireless support via wpa_supplicant.
+    wireless = {
+      enable = true; # Enables wireless support via wpa_supplicant.
+    };
 
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
