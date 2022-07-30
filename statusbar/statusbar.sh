0dabf055a77aac593104f2b87f0703d566d449f1
Sebastian Olsson <sebelino7@gmail.com>
2022-07-30 19:18:06 +0200

    feat(statusbar): Set up trayer properly

--- statusbar/statusbar.sh
+++ statusbar/statusbar.sh
@@ -1,23 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-pkill xmobar
+set -euo pipefail
 
-pkill trayer
+scriptdir="$(dirname "$(realpath "$0")")"
 
-xmobar &!
-
-sleep 0.2 # Race condition where xmobar might cover up trayer if the latter is faster to start
-
-trayer \
-    --SetDockType true \
-    --expand true \
-    --edge top \
-    --align right \
-    --width 10 \
-    --height 17 \
-    --distancefrom right \
-    --distance 150 \
-    --tint 0x000000 \
-    --transparent true \
-    --alpha 0 \
-    &!
+"$scriptdir/xmobar.sh" &!
+"$scriptdir/trayer.sh" &!
---
+++ statusbar/trayer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+pkill trayer
+
+trayer \
+    --SetDockType true \
+    --expand true \
+    --edge top \
+    --align left \
+    --width 4 \
+    --height 17 \
+    --tint 0x000000 \
+    --transparent true \
+    --alpha 0 \
+    &!
