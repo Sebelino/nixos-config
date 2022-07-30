17032099911ba21bcc25d65888dc8b3e92f07b59
Sebastian Olsson <sebelino7@gmail.com>
2022-07-30 18:32:48 +0200

    refactor(statusbar): Add newlines to trayer command

--- statusbar/statusbar.sh
+++ statusbar/statusbar.sh
@@ -8,4 +8,16 @@ xmobar &!
 
 sleep 0.2 # Race condition where xmobar might cover up trayer if the latter is faster to start
 
-trayer --SetDockType true --expand true --edge top --align right --width 10 --height 17 --distancefrom right --distance 150 --tint 0x000000 --transparent true --alpha 0 &!
+trayer \
+    --SetDockType true \
+    --expand true \
+    --edge top \
+    --align right \
+    --width 10 \
+    --height 17 \
+    --distancefrom right \
+    --distance 150 \
+    --tint 0x000000 \
+    --transparent true \
+    --alpha 0 \
+    &!
