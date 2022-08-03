0b950cc5c7bae1eb187f644daf47d27f76fc542d
Sebastian Olsson <sebelino7@gmail.com>
2022-08-03 07:56:12 +0200

    feat(bin): Switch from Google calendar -> Outlook for now

--- bin/calendar.sh
+++ bin/calendar.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-URL="https://calendar.google.com/calendar"
+GCALENDAR_URL="https://calendar.google.com/calendar"
+OUTLOOK_URL="https://outlook.office.com/calendar/view/week"
 
-google-chrome "-app=$URL" &!
-chromium "-app=$URL" &!
+url="$OUTLOOK_URL"
+
+google-chrome "-app=$url" &!
+chromium "-app=$url" &!
