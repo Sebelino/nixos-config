f3e4f01de4351c2d6c9e43e60d3cdceac6cc55e4
Sebastian Olsson <sebelino7@gmail.com>
2022-08-03 07:14:57 +0200

    feat(bin): Turn off more monitors

--- bin/one_screen.sh
+++ bin/one_screen.sh
@@ -7,6 +7,7 @@ set -x
 xrandr
 xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --primary \
        --output eDP-1 --mode 1920x1080 --pos 0x0 --primary \
+       --output HDMI-1 --off \
        --output HDMI-2 --off \
        --output HDMI-1-1 --off \
        --output HDMI-1-2 --off \
