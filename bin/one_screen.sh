921b082e83f305d7a3cfa0d06361559ad2e6ea37
Sebastian Olsson <sebelino7@gmail.com>
2022-10-21 18:42:27 +0200

    fix(bin): Add xrandr external monitors

--- bin/one_screen.sh
+++ bin/one_screen.sh
@@ -11,6 +11,7 @@ xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --primary \
        --output HDMI-2 --off \
        --output HDMI-1-1 --off \
        --output HDMI-1-2 --off \
+       --output HDMI-A-0 --off \
        --output DP-1 --off \
        --output DP-1-1 --off \
        --output DP-1-2 --off \
