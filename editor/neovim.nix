479fabd5188a871de1e01b0af8bc1c1e4f08c64f
Sebastian Olsson <sebelino7@gmail.com>
2023-08-16 17:57:48 +0200

    fix(editor): Correct vimrc path

--- editor/neovim.nix
+++ editor/neovim.nix
@@ -3,6 +3,6 @@
   viAlias = true;
   vimAlias = true;
   vimdiffAlias = true;
-  extraConfig = (builtins.readFile ./vimrc);
+  extraConfig = (builtins.readFile ./nvim/init.vim);
   plugins = with pkgs.vimPlugins; [ vim-nix vim-gitgutter vim-terraform ];
 }
