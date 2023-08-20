188ad6751e640fb74adefed5dd28ec20cedfc7bf
Sebastian Olsson <sebelino7@gmail.com>
2023-08-20 19:12:46 +0200

    fix(statusbar): Show ERROR if acpi is not installed

--- statusbar/charging.sh
+++ statusbar/charging.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
+
+complain() {
+    echo "<fc=#FF0000>ERROR</fc>"
+    exit 0
+}
+
+trap complain ERR
 
 status="$(acpi -a | awk -F': ' '{print $2}')"
 
-if [ $status = 'off-line' ]; then
+if [ "$status" = 'off-line' ]; then
     echo "<fc=#FFA000>■■■■■■</fc>"
 else
     echo "■■■■■■"
