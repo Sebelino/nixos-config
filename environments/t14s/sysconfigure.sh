f7fab643b55a2056bdd84834358c15503f6e70dd
Sebastian Olsson <sebelino7@gmail.com>
2023-09-01 18:09:59 +0200

    refactor: Move dupe bash functions to common script

--- environments/t14s/sysconfigure.sh
+++ environments/t14s/sysconfigure.sh
@@ -4,37 +4,6 @@ set -euo pipefail
 
 scriptdir="$(dirname "$(realpath "$0")")"
 
-symlink() {
-    src="$scriptdir/$1"
-    dst="$2"
-
-    dstdir="$(dirname "$dst")"
-    mkdir -p "$dstdir"
-
-    if [ -h "$dst" -a ! -e "$dst" ]; then
-        echo "Error: Symlink is broken: $dst"
-        exit 1
-    elif [ -L "$dst" ]; then
-        echo "Already symlinked: $dst"
-    elif [ -f "$dst" ]; then
-        echo "Error: Already exists and is a file: $dst" >&2
-        exit 1
-    else
-        echo "Symlinking: $src -> $dst"
-        ln -s "$src" "$dst"
-    fi
-}
-
-copy() {
-    src="$scriptdir/$1"
-    dst="$2"
-
-    if [ ! -f "$dst" ]; then
-        echo "Copying: $src -> $dst"
-        cp "$src" "$dst"
-    else
-        echo "Already present: $dst"
-    fi
-}
+source "${scriptdir}/../../lib/common.sh"
 
 symlink "sway" "$HOME/.config/sway"
