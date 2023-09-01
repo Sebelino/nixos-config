29bc5b9d40d2bd5c5e9c7b1df9ed7703289f96c3
Sebastian Olsson <sebelino7@gmail.com>
2023-09-01 18:04:30 +0200

    feat(environments/t14s): Symlink sway config

---
+++ environments/t14s/sysconfigure.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+scriptdir="$(dirname "$(realpath "$0")")"
+
+symlink() {
+    src="$scriptdir/$1"
+    dst="$2"
+
+    dstdir="$(dirname "$dst")"
+    mkdir -p "$dstdir"
+
+    if [ -h "$dst" -a ! -e "$dst" ]; then
+        echo "Error: Symlink is broken: $dst"
+        exit 1
+    elif [ -L "$dst" ]; then
+        echo "Already symlinked: $dst"
+    elif [ -f "$dst" ]; then
+        echo "Error: Already exists and is a file: $dst" >&2
+        exit 1
+    else
+        echo "Symlinking: $src -> $dst"
+        ln -s "$src" "$dst"
+    fi
+}
+
+copy() {
+    src="$scriptdir/$1"
+    dst="$2"
+
+    if [ ! -f "$dst" ]; then
+        echo "Copying: $src -> $dst"
+        cp "$src" "$dst"
+    else
+        echo "Already present: $dst"
+    fi
+}
+
+symlink "sway" "$HOME/.config/sway"
