eee99726da3d539a55c194308ddf2e27544e9d0a
Sebastian Olsson <sebelino7@gmail.com>
2023-09-02 14:06:42 +0200

    feat: Add more aliases

--- environments/t14s/shell/zsh/aliases.zsh
+++ environments/t14s/shell/zsh/aliases.zsh
@@ -14,6 +14,13 @@ alias giA="git add --patch"
 alias gid="git diff --no-ext-diff --cached"
 alias gir="git reset"
 
+# Log (l)
+alias gl="git log --topo-order --pretty=format:\"${_git_log_medium_format}\""
+
+# Push (p)
+alias gp="git push"
+alias gpf="git push --force-with-lease"
+
 # Working Copy (w)
 alias gws="git status --short"
 alias gwS="git status"
