e3469a8665083d6a070215a81292ec71790dca44
Sebastian Olsson <sebelino7@gmail.com>
2023-09-04 20:57:11 +0200

    fix: Remove output dir as well
    Or else there will be two ISOs in that directory the next day.

--- environments/t14s/sarch-rebuild.sh
+++ environments/t14s/sarch-rebuild.sh
@@ -7,5 +7,6 @@ set -x
 cd "$HOME/misc/iso/sarch/"
 
 sudo rm -r "$HOME/misc/iso/sarch/work"
+sudo rm -r "$HOME/misc/iso/sarch/output"
 
 sudo mkarchiso -v -w "$HOME/misc/iso/sarch/work" -o "$HOME/misc/iso/sarch/output" releng/
