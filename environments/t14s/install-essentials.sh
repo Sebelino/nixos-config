fa7e7de23274be8a8af14cc5d9077855141faadf
Sebastian Olsson <sebelino7@gmail.com>
2023-09-09 12:46:19 +0200

    fix: Make screensharing work in Chromium with Wayland + Pipewire

--- environments/t14s/install-essentials.sh
+++ environments/t14s/install-essentials.sh
@@ -5,3 +5,9 @@ set -euo pipefail
 scriptdir="$(dirname "$(realpath "$0")")"
 
 yay -S --needed --noconfirm - < "$scriptdir/pkgs-essentials.txt"
+
+# This package was somehow installed automatically. It interferes with xdg-desktop-portal(-wlr).
+if pacman -Qi "xdg-desktop-portal-gnome" 2>/dev/null; then
+    echo "Found xdg-desktop-portal-gnome. Uninstalling it..."
+    yay -R --noconfirm xdg-desktop-portal-gnome
+fi
