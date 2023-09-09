960828763049d3c015bc1045299e8f9d21758fb7
Sebastian Olsson <sebelino7@gmail.com>
2023-09-09 20:07:30 +0200

    feat: Install docker

--- environments/t14s/enable-daemons.sh
+++ environments/t14s/enable-daemons.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 sudo systemctl enable --now bluetooth
 sudo systemctl enable --now pacman-filesdb-refresh.timer
+sudo systemctl enable --now docker
 
 ## Unclear if this one is needed
 #systemctl enable --now --user xdg-desktop-portal-wlr
