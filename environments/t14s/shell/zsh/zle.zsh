db41f12019942b71fce8a68ad1b9c6ffa07c28da
Sebastian Olsson <sebelino7@gmail.com>
2023-09-10 16:57:16 +0200

    feat: Clear the line after copying it

--- environments/t14s/shell/zsh/zle.zsh
+++ environments/t14s/shell/zsh/zle.zsh
@@ -20,6 +20,7 @@ bindkey '^v' _git_commit_prep
 # ALT+U to copy the current line into clipboard
 _cmd_to_clip() {
   wl-copy -n <<< $BUFFER
+  BUFFER=''
 }
 zle -N _cmd_to_clip
 bindkey '\eu' _cmd_to_clip
