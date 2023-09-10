ae2b4a5d1f8123e42b498eee706997a67d0ee2fe
Sebastian Olsson <sebelino7@gmail.com>
2023-09-10 16:18:41 +0200

    feat: Improve unnecessarily complicated ZLE widget
    On second thought, it saves more time to just do CTRL+V and optionally
    erase one word with CTRL+W than to try to reverse-search the appropriate
    scope first.

--- environments/t14s/shell/zsh/zle.zsh
+++ environments/t14s/shell/zsh/zle.zsh
@@ -7,12 +7,12 @@
 # where '@' is the position of your cursor.
 # Useful when you reverse-search a previous 'gc -m' command and want to avoid
 # deleting the extra words by tapping CTRL+w repeatedly.
-_git_commit_clear() {
+_git_commit_prep() {
   line="$LBUFFER"
   parts=(${(s/:/)line})
   left_part="${parts[1]}: "
-  LBUFFER="$left_part"
+  LBUFFER='gc -m "chore: '
   RBUFFER='"'
 }
-zle -N _git_commit_clear
-bindkey '^v' _git_commit_clear
+zle -N _git_commit_prep
+bindkey '^v' _git_commit_prep
