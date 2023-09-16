20160253c2fcad3d0bb60f27b9ee9156b36393f9
Sebastian Olsson <sebelino7@gmail.com>
2023-09-16 19:55:56 +0200

    feat: Add unfinished create-vm.sh script

---
+++ environments/t14s/create-vm.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# The intention is to create a VM programmatically using this script instead of
+# using virt-manager. Note the --boot uefi parameter.
+
+set -euo pipefail
+
+set -x
+
+vm_name="win81"
+
+# Destroy VM if it already exists
+sudo virsh destroy "$vm_name" || true
+sudo virsh undefine --nvram "$vm_name" || true
+
+sudo virt-install \
+    --name "$vm_name" \
+    --ram=4096 \
+    --vcpus=2 \
+    --cpu host \
+    --hvm \
+    --disk path=/var/lib/libvirt/images/dualboot,size=64 \
+    --cdrom "$HOME/misc/iso/Win8.1_SingleLang_English_x64.iso" \
+    --osinfo win8.1 \
+    --network default \
+    --check path_in_use=off \
+    --boot uefi
