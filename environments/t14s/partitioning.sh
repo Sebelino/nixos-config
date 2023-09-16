017e7b22edc975c1b8759a652adb11c252626191
Sebastian Olsson <sebelino7@gmail.com>
2023-09-16 18:26:31 +0200

    feat: Add partitioning script

---
+++ environments/t14s/partitioning.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+set -x
+
+device_name="$(lsblk -J | jq -r '.blockdevices[] | select(.type == "disk") | .name')"
+
+device_file="/dev/${device_name}"
+
+# Assert exactly one disk was found
+ls "${device_file}"
+
+# Print all disks
+lsblk
+
+echo ""
+echo "Create a new partition in this disk?: --> ${device_file} <--"
+
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+sgdisk --new=:: "$device_file"
+
+# Print disk with partition
+lsblk "${device_file}"
