b283497ed0bf0cdac1a901922e4c5649b9232e97
Sebastian Olsson <sebelino7@gmail.com>
2023-09-17 11:05:33 +0200

    feat: Add ZLE widget for jumping back whole word

--- environments/t14s/shell/zsh/zle.zsh
+++ environments/t14s/shell/zsh/zle.zsh
@@ -10,6 +10,22 @@ _git_commit_prep() {
 zle -N _git_commit_prep
 bindkey '^v' _git_commit_prep
 
+# CTRL+H to jump back a whole word
+_jump_back_whole_world() {
+  # If there are no spaces to the left of the cursor, jump to the beginning
+  if [[ "$LBUFFER" != *' '* ]]; then
+    RBUFFER="${BUFFER}"
+    LBUFFER=''
+    return
+  fi
+  last_word="${LBUFFER##* }"
+  pos_last_word="$((${#LBUFFER} - ${#last_word} - 1))"
+  LBUFFER="${LBUFFER:0:${pos_last_word}}"
+  RBUFFER=" ${last_word}${RBUFFER}"
+}
+zle -N _jump_back_whole_world
+bindkey '^h' _jump_back_whole_world
+
 # ALT+U to copy the current line into clipboard
 _cmd_to_clip() {
   wl-copy -n <<< $BUFFER
