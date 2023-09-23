88475bf5ca49dcd10073f0b02de114e1e490b99d
Sebastian Olsson <sebelino7@gmail.com>
2023-09-23 15:31:58 +0200

    refactor: Extract solaar line to function

--- environments/t14s/mouse/keycontrol.sh
+++ environments/t14s/mouse/keycontrol.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 
 argument="$1"
 
+get_app_name() {
+    swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id'
+}
+
 # Keycodes for ydotool can be found in /usr/include/linux/input-event-codes.h
 
 kc_Hyper_L=12
@@ -18,7 +22,7 @@ case "$argument" in
         ydotool key $kc_Hyper_L:1 $kc_Shift_R:1 $key_T:1 $key_T:0 $kc_Shift_R:0 $kc_Hyper_L:0
     ;;
     'Middle Button')
-        app_name="$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id')"
+        app_name="$(get_app_name)"
         if [ "$app_name" = "chromium" ]; then
             # F5
             ydotool key $key_F5:1 $key_F5:0
