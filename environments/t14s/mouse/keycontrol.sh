eeba0497473d3cd6de0d0df6afba7e6b382311dc
Sebastian Olsson <sebelino7@gmail.com>
2023-09-23 15:41:36 +0200

    feat: Resurrect Anki Solaar rule

--- environments/t14s/mouse/keycontrol.sh
+++ environments/t14s/mouse/keycontrol.sh
@@ -14,6 +14,7 @@ kc_Hyper_L=12
 kc_Shift_R=57
 kc_F5=63
 kc_Ctrl=58
+kc_Space=51
 kc_W=17
 kc_T=33
 
@@ -22,6 +23,8 @@ case "$argument" in
         app_name="$(get_app_name)"
         if [ "$app_name" = "chromium" ]; then
             ydotool key $kc_Ctrl:1 $kc_W:1 $kc_W:0 $kc_Ctrl:0
+        elif [ "$app_name" = "anki" ]; then
+            ydotool key $kc_Space:1 $kc_Space:0
         else
             notify-send --urgency=low 'Kill window'
             ydotool key $kc_Hyper_L:1 $kc_Shift_R:1 $kc_T:1 $kc_T:0 $kc_Shift_R:0 $kc_Hyper_L:0
