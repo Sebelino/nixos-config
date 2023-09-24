982e8389f36a4a84f9061bb253c41f108d61d46a
Sebastian Olsson <sebelino7@gmail.com>
2023-09-24 19:35:02 +0200

    feat: Set up storage for VM

--- environments/t14s/create-vm.sh
+++ environments/t14s/create-vm.sh
@@ -9,12 +9,26 @@ scriptdir="$(dirname "$(realpath "$0")")"
 
 set -x
 
-image_pool_name="images"
+# Needed for virt-install by the libvirt-qemu user
+chmod go+x "$HOME/.local"
+chmod go+x "$HOME/.local/share"
 
+image_pool_name="images"
+image_name="dualboot"
+vol_name="dualboot"
+
+virsh vol-delete --vol "$vol_name" --pool "$image_pool_name"
+virsh pool-destroy --pool "$image_pool_name" || true
 virsh pool-delete --pool "$image_pool_name" || true
 virsh pool-undefine --pool "$image_pool_name" || true
 virsh pool-define --validate --file "${scriptdir}/vm/libvirt/pool-images.xml"
-virsh pool-build "$image_pool_name"
+virsh pool-build --pool "$image_pool_name"
+virsh pool-start --pool "$image_pool_name"
+virsh pool-autostart --pool "$image_pool_name"
+
+virsh vol-create --pool "$image_pool_name" --file "${scriptdir}/vm/libvirt/vol-dualboot.xml"
+
+image_path="$(virsh vol-path "$image_name" --pool images)"
 
 vm_name="win81"
 
@@ -22,14 +36,16 @@ vm_name="win81"
 virsh destroy "$vm_name" || true
 virsh undefine --nvram "$vm_name" || true
 
+iso="$HOME/misc/iso/Win8.1_SingleLang_English_x64.iso"
+
 virt-install \
     --name "$vm_name" \
     --ram=4096 \
     --vcpus=2 \
     --cpu host \
     --hvm \
-    --disk path=/var/lib/libvirt/images/dualboot,size=64 \
-    --cdrom "$HOME/misc/iso/Win8.1_SingleLang_English_x64.iso" \
+    --disk "path=${image_path},size=64" \
+    --cdrom "$iso" \
     --osinfo win8.1 \
     --network default \
     --check path_in_use=off \
