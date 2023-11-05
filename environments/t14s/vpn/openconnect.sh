9dcd3e8848a8338b44aea6a90f99c3242dd287af
Sebastian Olsson <sebelino7@gmail.com>
2023-11-05 10:38:07 +0100

    feat: Tell openconnect script to check if smartcard is inserted
    If not, I would get a PIN code prompt which I couldn't CTRL+C out of.

--- environments/t14s/vpn/openconnect.sh
+++ environments/t14s/vpn/openconnect.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # This script assumes your smartcard is inserted.
+# Requires following packages to be installed:
+# extra/pcsc-tools
 
 set -euo pipefail
 
@@ -11,6 +13,11 @@ CERT_TOKEN_URL_PATH=/tmp/certtokenurl.txt # Cache the token URL to avoid unneces
 TUN_IFACE="vpn0" # Will be shown in the output of `ip link show`
 
 set -x
+
+if ! pcsc_scan -c | grep "Card inserted"; then
+    echo "Please insert smart card."
+    exit 1
+fi
 
 if [ ! -d "/sys/class/net/${TUN_IFACE}" ]; then
     # If not already done, create a network interface for OpenConnect to use. Will disappear on reboot.
