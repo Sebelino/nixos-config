3bce36db4a06ac24e60c85dd15edcbf6b28a17b0
Sebastian Olsson <sebelino7@gmail.com>
2023-11-22 20:53:38 +0100

    feat: Support Wayland in solemak toggling script

--- bin/toggle_solemak.sh
+++ bin/toggle_solemak.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-out=`setxkbmap -query | grep solemak`
+set -euo pipefail
 
-if [ -z "$out" ]; then
-    setxkbmap solemak
+if [ "$XDG_CURRENT_DESKTOP" = "sway" ]; then
+    current_layout="$(swaymsg -t get_inputs | jq -r '.[].xkb_active_layout_name | select(. != null)' | sort -u)"
+    if [ "$current_layout" = "Solemak" ]; then
+        new_layout="se"
+    else
+        new_layout="solemak"
+    fi
+    swaymsg input type:keyboard xkb_layout "$new_layout"
+    echo "$new_layout"
 else
-    setxkbmap sesebel
+    out="$(setxkbmap -query | grep solemak)"
+    if [ -z "$out" ]; then
+        new_layout="solemak"
+    else
+        new_layout="sesebel"
+    fi
+    setxkbmap "$new_layout"
 fi
-
