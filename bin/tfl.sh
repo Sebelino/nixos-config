165e3beab26546004f3264f8b7e957cdc47410d3
Sebastian Olsson <sebelino7@gmail.com>
2023-12-06 10:45:47 +0100

    fix: Turn off annoying Teams notification ringing sounds

--- bin/tfl.sh
+++ bin/tfl.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 set -x
 
-teams-for-linux --enable-features=UseOzonePlatform --ozone-platform=wayland
+teams-for-linux \
+    --enable-features=UseOzonePlatform \
+    --ozone-platform=wayland \
+    --disableMeetingNotifications
 
 # You can confirm that the app is launched with Wayland by running `xlsclients`.
