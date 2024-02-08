688e1a3168d0cfa3f40e2a7760b06a32b9818499
Sebastian Olsson <sebelino7@gmail.com>
2024-02-08 22:54:41 +0100

    fix: Remove trailing %0A in urlencode script output

--- bin/urlencode.sh
+++ bin/urlencode.sh
@@ -2,4 +2,4 @@
 
 set -Eeuo pipefail
 
-python3 -c "import sys; from urllib.parse import quote_plus; print(quote_plus(sys.stdin.read()), end='');"
+python3 -c "import sys; from urllib.parse import quote_plus; print(quote_plus(sys.stdin.read().strip()), end='');"
