9c0495138d879dd33270acea6a6c06a2a676f9ca
Sebastian Olsson <sebelino7@gmail.com>
2024-02-25 21:46:10 +0100

    feat: Install tor-browser

--- environments/t14s/install-apps.sh
+++ environments/t14s/install-apps.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 scriptdir="$(dirname "$(realpath "$0")")"
 
+# Needed to install tor-browser-aur currently (2024-02-19)
+gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
+
 yay -S --needed --noconfirm - < "$scriptdir/pkgs-apps.txt"
 
 "$scriptdir/install-teams-for-linux.sh"
