ac66ee0328c0e98a4e1a738f35e4f8da95ca50c3
Sebastian Olsson <sebelino7@gmail.com>
2024-03-15 11:22:16 +0100

    feat: Fix jira-current-issue script bug

--- bin/jira-currently-assigned-issue.sh
+++ bin/jira-currently-assigned-issue.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 source "/home/sebelino/nixos-config/secrets/jira-token.sh"
 
-jira issue list -a "$(jira me)" --plain | tail -n +2 | cut -d $'\t' -f2
+jira issue list -a "$(jira me)" --status Ongoing --plain | tail -n 1 | cut -d $'\t' -f2
