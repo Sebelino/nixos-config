dd9f447fe0f7938a315b2640223eb47d6c393ab4
Sebastian Olsson <sebelino7@gmail.com>
2024-04-17 11:37:40 +0200

    feat: Enable backups of files edited with Neovim
    Painful lesson...

--- environments/t14s/editor/nvim/init.lua
+++ environments/t14s/editor/nvim/init.lua
@@ -417,6 +417,10 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+-- Backup files
+vim.opt.backupdir = '/home/sebelino/.local/share/nvim/backup//'
+vim.opt.backup = true
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
