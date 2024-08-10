ae16ffb4b8f063caa35c2d446dd6a7ac4f99aca9
Sebastian Olsson <sebelino7@gmail.com>
2024-08-10 10:33:00 +0200

    perf: Improve performance of install-apps.sh

--- environments/t14s/install-apps.sh
+++ environments/t14s/install-apps.sh
@@ -5,8 +5,15 @@ set -euo pipefail
 scriptdir="$(dirname "$(realpath "$0")")"
 
 # Needed to install tor-browser-aur currently (2024-02-19)
-gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
+gpg --quiet --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org > /dev/null
 
-yay -S --needed --noconfirm - < "$scriptdir/pkgs-apps.txt"
+missing_packages="$(comm -23 <(sort -u < "$scriptdir/pkgs-apps.txt") <(pacman -Q | cut -d' ' -f1 | sort -u))"
+
+if [ -z "$missing_packages" ]; then
+  echo "No packages to install."
+else
+  yay -S --needed --noconfirm "$missing_packages"
+fi
+
 
 "$scriptdir/install-teams-for-linux.sh"
