7613645fac1fdb45fe3f9993dbbe6778766af695
Sebastian Olsson <sebelino7@gmail.com>
2024-08-10 20:09:51 +0200

    feat: Remove underscore from function names
    Functions starting with underscore have a special meaning in zsh, so
    better not challenge fate here.

--- environments/t14s/shell/zsh/keybindings.zsh
+++ environments/t14s/shell/zsh/keybindings.zsh
@@ -3,15 +3,15 @@
 # In the terminal, typing CTRL+V will produce:
 # $ gc -m "feat: @"
 # with your cursor positioned at '@'.
-_git_commit_prep() {
+sebe_git_commit_prep() {
   LBUFFER='gc -m "feat: '
   RBUFFER='"'
 }
-zle -N _git_commit_prep
-bindkey '^v' _git_commit_prep
+zle -N sebe_git_commit_prep
+bindkey '^v' sebe_git_commit_prep
 
 # CTRL+H to jump back a whole word
-_jump_back_whole_world() {
+sebe_jump_back_whole_world() {
   # If there are no spaces to the left of the cursor, jump to the beginning
   if [[ "$LBUFFER" != *' '* ]]; then
     RBUFFER="${BUFFER}"
@@ -23,16 +23,16 @@ _jump_back_whole_world() {
   LBUFFER="${LBUFFER:0:${pos_last_word}}"
   RBUFFER=" ${last_word}${RBUFFER}"
 }
-zle -N _jump_back_whole_world
-bindkey '^h' _jump_back_whole_world
+zle -N sebe_jump_back_whole_world
+bindkey '^h' sebe_jump_back_whole_world
 
 # ALT+U to copy the current line into clipboard
-_cmd_to_clip() {
+sebe_cmd_to_clip() {
   wl-copy -n <<< $BUFFER
   BUFFER=''
 }
-zle -N _cmd_to_clip
-bindkey '\eu' _cmd_to_clip
+zle -N sebe_cmd_to_clip
+bindkey '\eu' sebe_cmd_to_clip
 
 lfcd() {
     cd "$(command lf -print-last-dir "$@")"
