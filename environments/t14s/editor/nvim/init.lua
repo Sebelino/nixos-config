9682c7e57ea9708d67c33e442190fc2fcd1a96b6
Sebastian Olsson <sebelino7@gmail.com>
2024-08-21 11:06:08 +0200

    refactor: Clarify lua fn with parentheses

--- environments/t14s/editor/nvim/init.lua
+++ environments/t14s/editor/nvim/init.lua
@@ -46,7 +46,7 @@ vim.g.maplocalleader = ' '
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     'git',
