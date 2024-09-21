193822fc62da1b0e667dbc189a8dcb1650964b4a
Ville Sebastian Olsson <sebelino7@gmail.com>
2024-09-21 22:14:45 +0200

    feat: Make anki todo count more visible

--- bin/anki_show_todo.sh
+++ bin/anki_show_todo.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 
 count="$(python3 "$HOME/nixos-config/bin/anki_get_todo.py")"
-notify-send --urgency=low "Anki todo count" "$count"
+notify-send --urgency=low "Anki todo count" "<span color='#57dafd' font='26px'>$count</span>"
