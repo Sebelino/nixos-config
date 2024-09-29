0268f83572135973bb8905179ef6dbf8fb7e0b01
Ville Sebastian Olsson <sebelino7@gmail.com>
2024-09-29 14:00:01 +0200

    feat: Use CTRL+E to autocomplete with copilot

--- environments/t14s/editor/nvim/init.lua
+++ environments/t14s/editor/nvim/init.lua
@@ -246,6 +246,10 @@ require('lazy').setup({
   {
     -- Github Copilot
     "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap('i', '<C-E>', 'copilot#Accept("<CR>")', {silent=true,expr=true})
+    end,
   },
   {
     -- LaTex development
