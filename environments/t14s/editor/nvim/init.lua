79f71acc02f391f9e3f98668df5ddef0cb93518f
Ville Sebastian Olsson <sebelino7@gmail.com>
2024-09-29 16:27:06 +0200

    feat: Disable Copilot for certain files

--- environments/t14s/editor/nvim/init.lua
+++ environments/t14s/editor/nvim/init.lua
@@ -387,6 +387,14 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+-- Disable Copilot for certain files
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = {"diary.txt"},
+  callback = function()
+    vim.b.copilot_enabled = false
+  end
+})
+
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
