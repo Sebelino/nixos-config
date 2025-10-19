c4edd87dcaf3dcec4c4c8bd006071611c1a314e4
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-10-19 08:23:02 +0200

    fix: Remove second beep from pomodoro timer

--- bin/timer.sh
+++ bin/timer.sh
@@ -5,16 +5,26 @@ set -Eeuo pipefail
 echo $$ > /tmp/timer.sh.pid
 
 mins="$1"
+mode="${2:-}"
 TIMER_FILE="/tmp/timer.sh.txt"
 PAUSE_FILE="/tmp/timer.sh.pause"
 SECONDS_LEFT=$((mins * 60))
 
 if [ -f "$PAUSE_FILE" ]; then
-  notify-send "⏸️ Error: Timer is already running (and paused)."
+  notify-send "⚠ Error: Timer is already running (and paused)."
+  paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
   exit 1
 fi
 
-notify-send "⏰ Timer set for $mins minutes."
+if [ "$mode" == "--skip-second-beep" ]; then
+    notify-send "⏰ Timer set for $mins minutes (no second beep)."
+elif [ "$mode" == "" ]; then
+    notify-send "⏰ Timer set for $mins minutes."
+else
+    notify-send "⚠ Invalid flag: ${mode}."
+    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
+    exit 1
+fi
 
 while [ $SECONDS_LEFT -gt 0 ]; do
     if [ -f "$PAUSE_FILE" ]; then
@@ -34,5 +44,8 @@ done
 echo "" > "$TIMER_FILE"
 notify-send "⏰ Time's up!"
 paplay /usr/share/sounds/freedesktop/stereo/complete.oga # Indicates soft deadline
-sleep 5
-paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga # Indicates hard deadline
+
+if [ "$mode" != "--skip-second-beep" ]; then
+    sleep 7
+    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga # Indicates hard deadline
+fi
