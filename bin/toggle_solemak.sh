7c2d941ec059529a787419777c8fef7fcaaf81ab
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-12-01 10:08:21 +0100

    feat: Toggle to proper Swedish keyboard layout
    With the previous Swedish keyboard layout, some keys were not working as
    expected, e.g. the space key still behaved like Shift.

--- bin/toggle_solemak.sh
+++ bin/toggle_solemak.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [ "$XDG_CURRENT_DESKTOP" = "sway" ]; then
     current_layout="$(swaymsg -t get_inputs | jq -r '.[].xkb_active_layout_name | select(. != null)' | sort -u)"
     if [ "$current_layout" = "Solemak" ]; then
-        new_layout="se"
+        new_layout="sesebel"
     else
         new_layout="solemak"
     fi
