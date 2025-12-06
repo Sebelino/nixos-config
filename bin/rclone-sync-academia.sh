85512790d19e4b35b1a95982e428bb67f4a177b6
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-12-06 13:50:33 +0100

    fix: rclone bisync without --rsync

--- bin/rclone-sync-academia.sh
+++ bin/rclone-sync-academia.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rclone bisync -P "$HOME/gdrive/academia" "google-drive:/academia" --drive-skip-gdocs --resync
+rclone bisync -P "$HOME/gdrive/academia" "google-drive:/academia" --drive-skip-gdocs
