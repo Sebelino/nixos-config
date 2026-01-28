28ea99dcbfd21cdc2ede74ef73af6b18cd0dceda
Ville Sebastian Olsson <sebelino7@gmail.com>
2026-01-28 14:24:37 +0100

    feat: Add karabiner config

--- environments/m4/sysconfigure.sh
+++ environments/m4/sysconfigure.sh
@@ -7,3 +7,5 @@ scriptdir="$(dirname "$(realpath "$0")")"
 source "${scriptdir}/../../lib/common.sh"
 
 symlink "display/aerospace/aerospace.toml" "$HOME/.aerospace.toml"
+symlink "keyboard/karabiner" "$HOME/.config/karabiner"
+
